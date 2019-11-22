cf72c7820391b4dae83b677a5b31f33da8a4de48
Carlos Barata <baratac@gmail.com>
2019-11-22 09:46:46 +0000

    Closing TGIF Sprint 1
    New desigh features to close TGIF Sprint 1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
+// ------- DOM utility functions ----
+
+let keepOneOpen = (elem, ref) => {
+    let elements = elem.querySelectorAll('[data-parent="#homeAccordion"]');
+    let foundElem;
+    let i = 0;
+
+    for (let xElem of elements) {
+        // console.log(i + ') ' + xElem.className);
+        if (xElem.className.indexOf('show') !== -1) {
+            foundElem = xElem;
+            break;
+        }
+        if (xElem.className.indexOf('collapsing') !== -1) {
+            foundElem = xElem;
+            break;
+        }
+        i++;
+    }
+    if (foundElem == undefined) {
+        //console.log('All sections all closed', elements);
+        //elements[0].classList.add('show');
+        ref.collapse('show');
+
+    }
+}
+
+
+
+
 // ----------- Functions ------------
 
 let createChamberCell = (content) => {
@@ -19,7 +49,7 @@ let createNameCell = (name, url) => {
 
 }
 
-let createSenateRow = (item) => {
+let createChamberRow = (item) => {
         let tRow = document.createElement("tr");
         let newCell = Object;
         let fullName = item.first_name + " ";
@@ -47,16 +77,53 @@ let createSenateRow = (item) => {
 console.dir(document.body);
 // console.log(document.body);
 
-let elem = document.getElementById("chamber-data");
-while (elem === null) {
-    elem = document.getElementById("chamber-data");
-}
 
-if (document.querySelector("body").baseURI.indexOf('index.html') !== -1) {
-    elem.innerHTML = JSON.stringify(data, null, 2);
+if (typeof data !== 'undefined') { // Only When data is available
+    let elem = document.getElementById("chamber-data");
+    while (elem === null) {
+        elem = document.getElementById("chamber-data");
+    }
+
+    if (document.querySelector("body").baseURI.indexOf('index.html') !== -1) {
+        elem.innerHTML = JSON.stringify(data, null, 2);
+    } else {
+        const tBody = elem.querySelector("tbody");
+        for (item of data.results[0].members) {
+            tBody.append(createChamberRow(item));
+        };
+    }
+
+    // For Senate/House pages -------------
+
+    elem = document.getElementById("content-title");
+    if (elem != null) {
+        elem.onclick = () => {
+            const imgElem = document.getElementById("content-title-arrow");
+            if (imgElem.currentSrc.indexOf('up.svg') === -1) {
+                imgElem.setAttribute('src', '../resources/icon-arrow-up.svg');
+                imgElem.setAttribute('srcset', '../resources/icon-arrow-up.svg');
+            } else {
+                imgElem.setAttribute('src', '../resources/icon-arrow-down.svg');
+                imgElem.setAttribute('srcset', '../resources/icon-arrow-down.svg');
+            }
+        }
+    } else {
+        console.log('No Content Title found...');
+    }
 } else {
-    const tBody = elem.querySelector("tbody");
-    for (item of data.results[0].members) {
-        tBody.append(createSenateRow(item));
-    };
+    // For Home Page -----------------------
+
+    elem = document.getElementById('homeAccordion');
+    if (elem != null) {
+        // $("#collapseOne").on("show.bs.collapse", () => console.log('1) show.bs.collapse'));
+        // $("#collapseOne").on("shown.bs.collapse", () => console.log('1) shown.bs.collapse'));
+        // $("#collapseOne").on("hide.bs.collapse", () => console.log('1) hide.bs.collapse'));
+        $("#collapseOne").on("hidden.bs.collapse", () => keepOneOpen(elem, $("#collapseTwo")));
+        // $("#collapseTwo").on("show.bs.collapse", () => console.log('2) show.bs.collapse'));
+        // $("#collapseTwo").on("shown.bs.collapse", () => console.log('2) shown.bs.collapse'));
+        // $("#collapseTwo").on("hide.bs.collapse", () => console.log('2) hide.bs.collapse'));
+        $("#collapseTwo").on("hidden.bs.collapse", () => keepOneOpen(elem, $("#collapseOne")));
+    } else {
+        console.log('--> content1 id not found...');
+    }
 }
